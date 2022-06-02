4b1e0cc65e9773efcef52b20c2a6965d2a2c1acb
Alexey Kozlov <alumedot@icloud.com>
2022-06-02 20:53:51 +0100

    feat: user signup, refactor App component to function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import React, { useEffect, useState, useCallback } from 'react';
+import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import Backdrop from './components/Backdrop/Backdrop';
@@ -13,52 +13,40 @@ import LoginPage from './pages/Auth/Login';
 import SignupPage from './pages/Auth/Signup';
 import './App.css';
 
-class App extends Component {
-  state = {
-    showBackdrop: false,
-    showMobileNav: false,
-    isAuth: true,
-    token: null,
-    userId: null,
-    authLoading: false,
-    error: null
+const App = () => {
+  const navigate = useNavigate();
+
+  const [showBackdrop, setShowBackdrop] = useState(false);
+  const [showMobileNav, setShowMobileNav] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [authLoading, setAuthLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const mobileNavHandler = (isOpen) => {
+    setShowMobileNav(isOpen);
+    setShowBackdrop(isOpen);
   };
 
-  componentDidMount() {
-    const token = localStorage.getItem('token');
-    const expiryDate = localStorage.getItem('expiryDate');
-    if (!token || !expiryDate) {
-      return;
-    }
-    if (new Date(expiryDate) <= new Date()) {
-      this.logoutHandler();
-      return;
-    }
-    const userId = localStorage.getItem('userId');
-    const remainingMilliseconds =
-      new Date(expiryDate).getTime() - new Date().getTime();
-    this.setState({ isAuth: true, token: token, userId: userId });
-    this.setAutoLogout(remainingMilliseconds);
-  }
-
-  mobileNavHandler = isOpen => {
-    this.setState({ showMobileNav: isOpen, showBackdrop: isOpen });
+  const backdropClickHandler = () => {
+    setShowBackdrop(false);
+    setShowMobileNav(false);
+    setError(null);
   };
 
-  backdropClickHandler = () => {
-    this.setState({ showBackdrop: false, showMobileNav: false, error: null });
-  };
-
-  logoutHandler = () => {
-    this.setState({ isAuth: false, token: null });
+  const logoutHandler = () => {
+    setIsAuth(false);
+    setToken(null);
     localStorage.removeItem('token');
     localStorage.removeItem('expiryDate');
     localStorage.removeItem('userId');
   };
 
-  loginHandler = (event, authData) => {
+  const loginHandler = (event, authData) => {
     event.preventDefault();
-    this.setState({ authLoading: true });
+    setAuthLoading(true);
+
     fetch('URL')
       .then(res => {
         if (res.status === 422) {
@@ -71,36 +59,46 @@ class App extends Component {
         return res.json();
       })
       .then(resData => {
-        console.log(resData);
-        this.setState({
-          isAuth: true,
-          token: resData.token,
-          authLoading: false,
-          userId: resData.userId
-        });
+        setIsAuth(true);
+        setToken(resData.token);
+        setAuthLoading(false);
+        setUserId(resData.userId);
+
         localStorage.setItem('token', resData.token);
         localStorage.setItem('userId', resData.userId);
+
         const remainingMilliseconds = 60 * 60 * 1000;
         const expiryDate = new Date(
           new Date().getTime() + remainingMilliseconds
         );
         localStorage.setItem('expiryDate', expiryDate.toISOString());
-        this.setAutoLogout(remainingMilliseconds);
+        setAutoLogout(remainingMilliseconds);
       })
       .catch(err => {
         console.log(err);
-        this.setState({
-          isAuth: false,
-          authLoading: false,
-          error: err
-        });
+        setIsAuth(false);
+        setAuthLoading(false);
+        setError(err);
       });
   };
 
-  signupHandler = (event, authData) => {
+  const signupHandler = (
+    event,
+    { signupForm: { email, password, name } }
+  ) => {
     event.preventDefault();
-    this.setState({ authLoading: true });
-    fetch('URL')
+    setAuthLoading(true);
+    fetch('http://localhost:8080/auth/signup', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value,
+        name: name.value
+      })
+    })
       .then(res => {
         if (res.status === 422) {
           throw new Error(
@@ -113,49 +111,90 @@ class App extends Component {
         }
         return res.json();
       })
-      .then(resData => {
-        console.log(resData);
-        this.setState({ isAuth: false, authLoading: false });
-        this.props.history.replace('/');
+      .then(_resData => {
+        setIsAuth(false);
+        setAuthLoading(false);
+        navigate('/');
       })
       .catch(err => {
         console.log(err);
-        this.setState({
-          isAuth: false,
-          authLoading: false,
-          error: err
-        });
+        setIsAuth(false);
+        setAuthLoading(false);
+        setError(err);
       });
   };
 
-  setAutoLogout = milliseconds => {
+  const setAutoLogout = useCallback(milliseconds => {
     setTimeout(() => {
-      this.logoutHandler();
+      logoutHandler();
     }, milliseconds);
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
   };
 
-  errorHandler = () => {
-    this.setState({ error: null });
-  };
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const expiryDate = localStorage.getItem('expiryDate');
+    if (!token || !expiryDate) {
+      return;
+    }
+    if (new Date(expiryDate) <= new Date()) {
+      logoutHandler();
+      return;
+    }
+    const userId = localStorage.getItem('userId');
+    const remainingMilliseconds =
+      new Date(expiryDate).getTime() - new Date().getTime();
 
-  render() {
-    let routes = (
+    setIsAuth(true);
+    setToken(token);
+    setUserId(userId);
+
+    setAutoLogout(remainingMilliseconds);
+  }, [setAutoLogout]);
+
+  let routes = (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <LoginPage
+            onLogin={loginHandler}
+            loading={authLoading}
+          />
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <SignupPage
+            onSignup={signupHandler}
+            loading={authLoading}
+          />
+        }
+      />
+      <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+      />
+    </Routes>
+  );
+
+  if (isAuth) {
+    routes = (
       <Routes>
         <Route
           path="/"
-          element={
-            <LoginPage
-              onLogin={this.loginHandler}
-              loading={this.state.authLoading}
-            />
-          }
+          element={<FeedPage userId={userId} token={token} />}
         />
         <Route
-          path="/signup"
+          path="/:postId"
           element={
-            <SignupPage
-              onSignup={this.signupHandler}
-              loading={this.state.authLoading}
+            <SinglePostPage
+              userId={userId}
+              token={token}
             />
           }
         />
@@ -165,59 +204,37 @@ class App extends Component {
         />
       </Routes>
     );
-    if (this.state.isAuth) {
-      routes = (
-        <Routes>
-          <Route
-            path="/"
-            element={<FeedPage userId={this.state.userId} token={this.state.token} />}
-          />
-          <Route
-            path="/:postId"
-            element={
-              <SinglePostPage
-                userId={this.state.userId}
-                token={this.state.token}
-              />
-            }
-          />
-          <Route
-            path="*"
-            element={<Navigate to="/" replace />}
-          />
-        </Routes>
-      );
-    }
-    return (
-      <>
-        {this.state.showBackdrop && (
-          <Backdrop onClick={this.backdropClickHandler} />
-        )}
-        <ErrorHandler error={this.state.error} onHandle={this.errorHandler} />
-        <Layout
-          header={
-            <Toolbar>
-              <MainNavigation
-                onOpenMobileNav={this.mobileNavHandler.bind(this, true)}
-                onLogout={this.logoutHandler}
-                isAuth={this.state.isAuth}
-              />
-            </Toolbar>
-          }
-          mobileNav={
-            <MobileNavigation
-              open={this.state.showMobileNav}
-              mobile
-              onChooseItem={this.mobileNavHandler.bind(this, false)}
-              onLogout={this.logoutHandler}
-              isAuth={this.state.isAuth}
-            />
-          }
-        />
-        {routes}
-      </>
-    );
   }
+
+  return (
+    <>
+      {showBackdrop && (
+        <Backdrop onClick={backdropClickHandler} />
+      )}
+      <ErrorHandler error={error} onHandle={errorHandler} />
+      <Layout
+        header={
+          <Toolbar>
+            <MainNavigation
+              onOpenMobileNav={mobileNavHandler.bind(this, true)}
+              onLogout={logoutHandler}
+              isAuth={isAuth}
+            />
+          </Toolbar>
+        }
+        mobileNav={
+          <MobileNavigation
+            open={showMobileNav}
+            mobile
+            onChooseItem={mobileNavHandler.bind(this, false)}
+            onLogout={logoutHandler}
+            isAuth={isAuth}
+          />
+        }
+      />
+      {routes}
+    </>
+  );
 }
 
 export default App;
